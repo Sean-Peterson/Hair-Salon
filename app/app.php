65bea9cc300104324d2e1847a59d0aa2a924c382
Sean Peterson <seanpeterson11@gmail.com>
2017-02-24 11:11:58 -0800

    add web/index.php app/app.php and get / post routes for index.html.twig

---
+++ app/app.php
@@ -0,0 +1,36 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Stylist.php";
+
+
+    $app = new Silex\Application();
+
+    $app['debug'] = true;
+
+
+    $server = 'mysql:host=localhost:3306;dbname=hair_salon';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    $app->post("/", function() use ($app) {
+        $new_stylist = new Stylist($_POST['new_stylist']);
+        $new_stylist->save(); 
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    return $app;
+?>
